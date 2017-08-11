c67a5a67b7998d919d6ea898eb0aa39f71e19fb4
Nir Mekin <nir.mekin@gmail.com>
2017-08-11 19:51:19 +0300

    first commit

---
+++ Submarines/Assets/Scripts/SC_Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void main_btn_singleplyer_logic(){
+        Debug.Log("Test");
+    }
+
+    public void main_btn_multiplayer_logic()
+    {
+        Debug.Log("Test");
+    }
+
+    public void main_btn_info_logic()
+    {
+        Debug.Log("Test");
+    }
+}
